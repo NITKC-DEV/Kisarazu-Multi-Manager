868fb23f9c5e8eb2df2c22ffdfbe8db07203166e
starkoka <j22437@inc.kisarazu.ac.jp>
2022-09-15 12:43:37 +0900

    原神デイリー通知を実装

---
+++ commands/genshintimer.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports =
+    [
+
+        {
+            data: new SlashCommandBuilder()
+                .setName('genshintimer')
+                .setDescription('About genshin-timer discord bot'),
+            async execute(interaction) {
+                const about = new EmbedBuilder()
+                    .setColor(0x27668D)
+                    .setTitle('About Genshin-timer Discord bot')
+                    .setAuthor({
+                        name: 'Genshin-timer',
+                        icon_url: 'https://pbs.twimg.com/media/FcdR7aIaIAE75Uu?format=png&name=large',
+                        url: 'https://github.com/starkoka/Genshin-Timer',
+                    })
+                    .setDescription('原神の様々な通知を行うことができるタイマーbotです\n\n')
+                    .addFields(
+                        [
+                            {
+                                name: '​\nデイリー通知機能',
+                                value: 'デイリーミッションの更新や、週ボスの更新等をお知らせします。\n',
+                            },
+                            {
+                                name: '​\nリポップ通知機能',
+                                value: 'coming soon...\n',
+                            },
+                            {
+                                name: '​\n樹脂回復通知機能',
+                                value: 'coming soon...\n',
+                            },
+                        ],
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: 'Developed by NITKC22s server Admin' });
+                await interaction.reply({ embeds: [about] });
+            },
+        },
+    ]
+
+;
+
+
+const commands = require('../botmain')
