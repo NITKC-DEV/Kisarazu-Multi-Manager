a741429296f6c70b0d58eaf425d7392ff4bf60c8
starkoka <j22437@inc.kisarazu.ac.jp>
2022-09-16 17:11:50 +0900

    v1.1.1 release(several bug fixes)

--- commands/general.js
+++ commands/general.js
@@ -41,7 +41,7 @@ module.exports =
                         [
                             {
                                 name: 'バージョン情報',
-                                value: 'v1.0.2 ',
+                                value: 'v1.1.1 ',
                             },
                             {
                                 name: '開発者',
@@ -67,41 +67,7 @@ module.exports =
             },
 
         },
-        {
-            data: new SlashCommandBuilder()
-                .setName('genshintimer')
-                .setDescription('About genshin-timer discord bot'),
-            async execute(interaction) {
-                const about = new EmbedBuilder()
-                    .setColor(0x27668D)
-                    .setTitle('About Genshin-timer Discord bot')
-                    .setAuthor({
-                        name: 'Genshin-timer',
-                        icon_url: 'https://pbs.twimg.com/media/FcdR7aIaIAE75Uu?format=png&name=large',
-                        url: 'https://github.com/starkoka/Genshin-Timer',
-                    })
-                    .setDescription('原神の様々な通知を行うことができるタイマーbotです\n\n')
-                    .addFields(
-                        [
-                            {
-                                name: '​\nデイリー通知機能',
-                                value: 'デイリーミッションの更新や、週ボスの更新等をお知らせします。\n',
-                            },
-                            {
-                                name: '​\nリポップ通知機能',
-                                value: 'coming soon...\n',
-                            },
-                            {
-                                name: '​\n樹脂回復通知機能',
-                                value: 'coming soon...\n',
-                            },
-                        ],
-                    )
-                    .setTimestamp()
-                    .setFooter({ text: 'Developed by NITKC22s server Admin' });
-                await interaction.reply({ embeds: [about] });
-            },
-        },
+
 
     ]
 
