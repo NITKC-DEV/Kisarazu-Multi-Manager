f9f424898eaec55b89d94f9d3bc21dc7c75d6043
starkoka <j22437@inc.kisarazu.ac.jp>
2022-10-07 00:22:16 +0900

    機械工学科月曜日の時間割を実装

--- commands/timetable.js
+++ commands/timetable.js
@@ -35,21 +35,59 @@ module.exports =[
             ),
 
         async execute(interaction) {
-            const commands = require('../botmain')
-            const embed = new EmbedBuilder()
-                .setColor(0x00A0EA)
-                .setTitle('ヘルプ')
-                .setAuthor({
-                    name: "\u200b",
-                    iconURL: 'https://pbs.twimg.com/media/FcoDQ9zaIAUL08j?format=png&name=small',
-                    url: 'https://discord.gg/mwyC8PTcXa'
-                })
-                .setDescription('現在実装されているコマンド一覧です')
-                .addFields(
-                    commands.map(e => ({ name: '/' + e.data.name, value: e.data.description }))
-                )
-                .setTimestamp()
-                .setFooter({ text: 'Developed by NITKC22s server Admin' });
+            let embed;
+            if(interaction.options.getString('曜日')=='1'){
+                if(interaction.options.getString('学科')=='M'){
+                    embed = new EmbedBuilder()
+                        .setColor(0x00A0EA)
+                        .setTitle('機械工学科 時間割')
+                        .setAuthor({
+                            name: "木更津22s統合管理BOT",
+                            iconURL: 'https://pbs.twimg.com/media/FcoDQ9zaIAUL08j?format=png&name=small',
+                            url: 'https://discord.gg/mwyC8PTcXa'
+                        })
+                        .setDescription('月曜日の時間割です。\n ※休講や、授業変更等がある可能性があります。各自で確認してください')
+                        .addFields(
+                            [
+                                {
+                                    name: '基礎数学Ⅱ',
+                                    value:'担当教員:関口 昌由 \n授業場所:1年機械工学科教室',
+                                },
+                                {
+                                    name: '工学実験ⅠB',
+                                    value:'担当教員:小田 功,高橋 美喜男,松井 翔太 \n授業場所:実習工場',
+                                },
+                                {
+                                    name: '図学製図Ⅱ',
+                                    value:'担当教員:松井 翔太 \n授業場所:1年機械工学科教室',
+                                },
+                                {
+                                    name: '課題学習時間',
+                                    value:'授業場所:1年機械工学科教室ほか ',
+                                },
+
+                            ]
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: 'Developed by NITKC22s server Admin' });
+
+                }
+            }
+            else if(interaction.options.getString('曜日')=='2'){
+
+            }
+            else if(interaction.options.getString('曜日')=='3'){
+
+            }
+            else if(interaction.options.getString('曜日')=='4'){
+
+            }
+            else if(interaction.options.getString('曜日')=='5'){
+
+            }
+            else{
+
+            }
             await interaction.reply({ embeds: [embed] });
         },
     },
