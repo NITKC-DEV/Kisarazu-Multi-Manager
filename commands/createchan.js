0470a514914f039e1a053e683c873ec1a1952173
Kou-Ro <koushi.rockv@gmail.com>
2022-10-24 23:50:10 +0900

    チャンネル作成機能実装

--- commands/createchan.js
+++ commands/createchan.js
@@ -1,11 +1,14 @@
-const { SlashCommandBuilder, Guild}=require('discord.js');
+const { SlashCommandBuilder}=require('discord.js');
 
+//ここから先の[]が"../botmain.js/スラッシュコマンド登録"にてcommandsに代入
 module.exports=
     [
         {
+            //スラッシュコマンドの定義
             data:new SlashCommandBuilder()
                 .setName('createchan')
                 .setDescription('チャンネルの作成')
+                //チャンネル名を入力 -> string
                 .addStringOption(option =>
                     option
                         .setName('チャンネル名')
@@ -13,21 +16,37 @@ module.exports=
                         .setRequired(true)
 
                 )
-                .addNumberOption(option =>
+                //チャンネルを作成するカテゴリを指定し、そのカテゴリのIDを取得 -> string
+                .addStringOption(option =>
                     option
                         .setName('カテゴリ')
                         .setDescription('チャンネルを作成するカテゴリを指定します')
                         .setRequired(true)
                         .addChoices(
-                            {name:'専門科目を語る会',value:1033563012470157403},
+                            {name:'専門科目を語る会',value:'1033563012470157403'},
                         )
                 )
+                //チャンネルに対応したロールを作成をするかどうかを指定 ->boolean
                 .addBooleanOption(option =>
                      option
                         .setName('ロールの作成')
                         .setDescription('チャンネルに対応したロールを作成するかを指定します')
                         .setRequired(true)
-                )
+                ),
+
+            //"../botmain.js-l42"より、スラッシュコマンド実行時の情報"interaction"を"interactionCopy"にコピー
+            async execute(interactionCopy)
+            {
+                //入力されたチャンネル名を扱いやすいように単独の変数に変換
+                const channelName = interactionCopy.options.getString('チャンネル名');
+
+                //選択されたカテゴリのIDを扱いやすいように単独の変数に変換
+                const categoryID= interactionCopy.options.getString('カテゴリ');
+
+                //                                           チャンネル名     カテゴリのID
+                await interactionCopy.guild.channels.create({name:channelName,parent:categoryID});
+                await interactionCopy.reply("Created!!!!!");
+            }
 
         }
     ]
