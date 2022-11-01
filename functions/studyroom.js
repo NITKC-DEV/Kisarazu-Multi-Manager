94d8165013fd208aa1b6a8d662e0bbefe63a4073
starkoka <j22437@inc.kisarazu.ac.jp>
2022-11-01 17:48:52 +0900

    滞在時間記録

--- functions/studyroom.js
+++ functions/studyroom.js
@@ -1,12 +1,24 @@
 /*自習室機能 VC部分*/
+const fs = require('fs');
+const date = JSON.parse(fs.readFileSync('./studyroom.json', 'utf8'));
+
 exports.func = function studyroom(oldState, newState){
+    let time = new Date();
+    let UNIX=time.getTime()/1000; //UNIXTime
+    let user=date.date.find(date => date.uid === oldState.id); /*その人のデータ*/
     if(oldState.channel===null){
-        console.log(oldState+"さんがVCに入りました！大歓迎！");
+        console.log(user.name+"さんがVCに入りました！大歓迎！");
+        date.date.find(date => date.uid === oldState.id).lastJoin = UNIX; //参加した時刻を書き込み
     }
     else if(newState.channel===null){
-        console.log(oldState+"さんがVCから離れたらしい！もう二度と来んじゃねーぞ");
+        date.date.find(date => date.uid === oldState.id).StudyAll += UNIX-date.date.find(date => date.uid === oldState.id).lastJoin;
+        console.log(user.name+"さんがVCから離れたらしい！滞在時間:"+(UNIX-date.date.find(date => date.uid === oldState.id).lastJoin) + "合計滞在時間" + date.date.find(date => date.uid === oldState.id).StudyAll);
+
+
     }
     else{
-        console.log(oldState+"さんがVCを変更しました！ちぇ〜んじ");
+        console.log(user.name+"さんがVCを変更しました！ちぇ〜んじ");
     }
+
+    fs.writeFileSync('./studyroom.json', JSON.stringify(date,null ,"\t")); //json書き出し
 }
