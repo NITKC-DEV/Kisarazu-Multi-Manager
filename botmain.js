8872d8e2b36b459a7a9d5a9fc16e47542b954637
starkoka <j22437@inc.kisarazu.ac.jp>
2023-01-26 22:37:49 +0900

    寮食・風呂前半後半通知システム実装

--- botmain.js
+++ botmain.js
@@ -1,4 +1,6 @@
-const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder, SlashCommandBuilder, Events,Message,ActionRowBuilder,SelectMenuBuilder} = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder, SlashCommandBuilder, Events,Message,ActionRowBuilder,SelectMenuBuilder,
+    version
+} = require('discord.js');
 const config = require('./environmentConfig')
 let ccconfig=require("./CCConfig.json");
 const timetableBuilder  = require('./timetable/timetableUtils');
@@ -10,6 +12,7 @@ const path = require('path');
 const fs = require('fs');
 const cron = require('node-cron');
 require('date-utils');
+const packageVer = require("./package.json");
 dotenv.config();
 const client = new Client({
     intents: [
@@ -337,6 +340,66 @@ cron.schedule('0 20 * * 0,1,2,3,4', async () => {
         (await (client.channels.cache.get(config.C) ?? await client.channels.fetch(config.C))
             .send({ embeds: [timetableBuilder(Classes.C, dayOfWeek)] }));
     }
+});
+
+//寮食前半後半通知
+cron.schedule('30 7 1 * *', () => {
+    let dt = new Date();
+    let month = dt.getMonth();
+    let embed
+    if(month === 0 || month === 4 || month === 6 || month === 7 || month === 10 ){ //月は何故か-1する
+        embed = new EmbedBuilder()
+            .setColor(0x00A0EA)
+            .setTitle('寮食・風呂切り替え通知')
+            .setAuthor({
+                name: "木更津22s統合管理BOT",
+                iconURL: 'https://media.discordapp.net/attachments/1004598980929404960/1039920326903087104/nitkc22io-1.png',
+                url: 'https://github.com/NITKC22s/bot-main'
+            })
+            .setDescription('寮食・風呂の前半後半が入れ替わりました')
+            .addFields(
+                [
+                    {
+                        name: '前半',
+                        value: 'Bグループ(環境都市工学科、情報工学科、電子制御工学科の半分)',
+                    },
+                    {
+                        name: '後半',
+                        value: 'Aグループ(機械工学科、電気電子工学科、電子制御工学科の半分)',
+
+                    },
+                ]
+            )
+            .setTimestamp()
+            .setFooter({ text: 'Developed by NITKC22s server Admin' });
+    }
+    else if(month === 1 || month === 3 || month === 5 || month === 9 || month === 11 ){
+        embed = new EmbedBuilder()
+            .setColor(0x00A0EA)
+            .setTitle('寮食・風呂切り替え通知')
+            .setAuthor({
+                name: "木更津22s統合管理BOT",
+                iconURL: 'https://media.discordapp.net/attachments/1004598980929404960/1039920326903087104/nitkc22io-1.png',
+                url: 'https://github.com/NITKC22s/bot-main'
+            })
+            .setDescription('寮食・風呂の前半後半が入れ替わりました')
+            .addFields(
+                [
+                    {
+                        name: '前半',
+                        value: 'Aグループ(機械工学科、電気電子工学科、電子制御工学科の半分)',
+                    },
+                    {
+                        name: '後半',
+                        value: 'Bグループ(環境都市工学科、情報工学科、電子制御工学科の半分)',
+
+                    },
+                ]
+            )
+            .setTimestamp()
+            .setFooter({ text: 'Developed by NITKC22s server Admin' });
+    }
+    client.channels.cache.get(config.ryou).send({ embeds: [embed] })
 });
 
 
