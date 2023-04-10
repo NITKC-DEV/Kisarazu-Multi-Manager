d3e6093a5396cce3b9eb9f83d1bb81cc58d0ba5f
starkoka <j22437@inc.kisarazu.ac.jp>
2023-04-10 15:10:37 +0900

    スラッシュコマンド実行失敗時にエラーを吐くようにしよう

--- botmain.js
+++ botmain.js
@@ -64,6 +64,7 @@ client.on("interactionCreate", async (interaction) => {
         await command.execute(interaction);
     } catch (error) {
         system.error(error);
+        system.error("スラッシュコマンド実行時エラー");
         await interaction.reply({ content: 'おっと、想定外の事態が起きちゃった。管理者に連絡してくれ。', ephemeral: true });
     }
 });
