023be07f1e16ec56f3551dc15910e6c3b4304c3e
starkoka <j22437@inc.kisarazu.ac.jp>
2023-04-16 23:36:59 +0900

    まだ余裕でエラー履いてるけど、とりあえずfilterの自由度をあげたりする修正をした

--- functions/db.js
+++ functions/db.js
@@ -7,36 +7,31 @@ const system = require('../functions/logsystem.js');
  * データベースからデータを取得する
  * @param dbName 取得先データベース名
  * @param collectionName 取得先コレクション名
- * @param label labelキーのフィルター
+ * @param filter フィルターを指定
  * @returns オブジェクト型の配列
  */
 
-exports.getDatabase = async function (dbName,collectionName,label) {
+exports.getDatabase = async function (dbName,collectionName,filter) {
     const dbClient = new MongoClient(config.db, { serverApi: ServerApiVersion.v1 });
     const collection = dbClient.db(dbName).collection(collectionName);
 
-    return await collection.find({label: label}).toArray()
+    return await collection.find(filter).toArray()
 }
 
 /***
  * データベースからデータを取得する
  * @param dbName 取得先データベース名
  * @param collectionName 取得先コレクション名
- * @param label labelキーのフィルター
+ * @param filter フィルターを指定
  * @param update update operatorを用いた更新内容の記述
  */
-exports.updateDB = async function run(dbName,collectionName,label,update) {
+exports.updateDB = async function run(dbName,collectionName,filter,update) {
     const dbClient = new MongoClient(config.db, { serverApi: ServerApiVersion.v1 });
     try {
         const database = dbClient.db(dbName);
         const collection = database.collection(collectionName);
 
-        const result = await collection.updateOne(
-            {
-                label: label
-            },
-            update
-        )
+        const result = await collection.updateOne(filter,update)
         system.log(`${dbName}.${collectionName}を更新`,`db操作実行`);
     } finally {
         await dbClient.close();
