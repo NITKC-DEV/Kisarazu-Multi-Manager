6fc18935d528b3ad9b4efe569fa714b78df32cfb
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-05 04:23:41 +0900

    prettier適用

--- environmentConfig.js
+++ environmentConfig.js
@@ -1,8 +1,7 @@
+/** @format */
+
 const Path = require("path");
-const configPath = Path.resolve(
-  __dirname,
-  process.env.NODE_ENV == "production" ? "./config.json" : "./config.dev.json"
-);
+const configPath = Path.resolve(__dirname, process.env.NODE_ENV == "production" ? "./config.json" : "./config.dev.json");
 const config = require(configPath);
 module.exports = config;
 module.exports.configPath = configPath;
