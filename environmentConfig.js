2fff480d7a101d55090b8b4f187d87f1161f741f
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-05 04:24:44 +0900

    newline-after-import対応

--- environmentConfig.js
+++ environmentConfig.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const Path = require("path");
+
 const configPath = Path.resolve(__dirname, process.env.NODE_ENV == "production" ? "./config.json" : "./config.dev.json");
 const config = require(configPath);
 module.exports = config;
