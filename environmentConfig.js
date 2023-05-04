db9ce1c7372bc00ba634dc188462bd86d9d60964
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-05-05 04:31:36 +0900

    厳密等価演算子の使用を強制

--- environmentConfig.js
+++ environmentConfig.js
@@ -2,7 +2,7 @@
 
 const Path = require("path");
 
-const configPath = Path.resolve(__dirname, process.env.NODE_ENV == "production" ? "./config.json" : "./config.dev.json");
+const configPath = Path.resolve(__dirname, process.env.NODE_ENV === "production" ? "./config.json" : "./config.dev.json");
 const config = require(configPath);
 module.exports = config;
 module.exports.configPath = configPath;
