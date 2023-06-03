6e5781a722063506153067e4034b1cd0a29a8ba3
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-03 19:56:44 +0900

    auto-dashboardをまだ生成していないサーバーで実行した場合、DBに登録して生成するコマンドに仕様変更

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const {configPath} = require("../environmentConfig");
 const dashboard = require('../functions/dashboard.js');
 const db = require('../functions/db.js');
+const system = require("../functions/logsystem");
+const {setTimeout} = require("node:timers/promises");
 module.exports =
     [
         {
@@ -89,33 +91,39 @@ module.exports =
         {
             data: new SlashCommandBuilder()
                 .setName('auto-dashboard')
-                .setDescription('自動更新されるダッシュボードを選択')
-                .setDefaultMemberPermissions(1<<3)
-                .addStringOption(option =>
-                    option
-                        .setName('ダッシュボードid')
-                        .setDescription('メッセージIDを入力')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('チャンネルid')
-                        .setDescription('チャンネルIDを入力')
-                        .setRequired(true)
-                )
-                .addStringOption(option =>
-                    option
-                        .setName('ギルドid')
-                        .setDescription('ギルドIDを入力')
-                        .setRequired(true)
-                ),
+                .setDescription('自動更新されるダッシュボードを生成')
+                .setDefaultMemberPermissions(1<<3),
 
             async execute(interaction) {
-                const data = JSON.parse(fs.readFileSync(configPath, 'utf8'))  //ここで読み取り
+                await interaction.deferReply({ ephemeral: true })
+                const data = await db.getDatabase("main","dashboard",{guild:String(interaction.guildId)});
+                if(data.length > 0){
+                    console.log("存在する")
+                }
+                else{
+                    const embed = await dashboard.generation(interaction.guild)
+                    const board = await interaction.channel.send({ embeds: [embed] });
+                    await db.add("main","dashboard",{
+                        guild: String(interaction.guildId),
+                        board: String(board.id)
+                    })
+                    const replyOptions=time=>{return{content: 'ダッシュボードを生成し、自動更新を有効にしました。\n(このメッセージは'+time+'秒後に自動で削除されます)', ephemeral:true};};
+                    await interaction.editReply(replyOptions(5));
+                    //5秒カウントダウンしたのちに返信を削除
+                    for(let i=5;i>0;i--){
+                        await interaction.editReply(replyOptions(i));
+                        await setTimeout(1000);
+                    }
+                    await interaction.deleteReply();
+                }
+
+
+
+                /*
                 data.dashboard = [interaction.options.data[0].value,interaction.options.data[1].value,interaction.options.data[2].value]
                 fs.writeFileSync(configPath, JSON.stringify(data,null ,"\t"))
                 await interaction.reply({ content: `メッセージID:${data.dashboard[0]} を、自動更新ダッシュボードに設定しました。`, ephemeral: true });
-
+                */
             },
         },
     ]
