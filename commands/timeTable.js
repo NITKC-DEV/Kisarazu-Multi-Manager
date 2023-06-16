1f27f524b8057b64bc6e7f8e3663f0eb7d77b346
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-17 02:01:29 +0900

    comment-timetableコマンドを追加して、とりあえず仮のプログラムを配置(動作はしない)

--- commands/timeTable.js
+++ commands/timeTable.js
@@ -1,8 +1,12 @@
-const {SlashCommandBuilder, StringSelectMenuBuilder,EmbedBuilder,ButtonBuilder,} = require('discord.js');
+const {SlashCommandBuilder, StringSelectMenuBuilder,EmbedBuilder,ButtonBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    ActionRowBuilder,} = require('discord.js');
 const  timetable  = require('../functions/ttGeneration.js');
 const fs = require('fs');
 const {configPath}=require("../environmentConfig");
 const db = require('../functions/db.js');
+const {setTimeout} = require("node:timers/promises");
 
 const departmentData = [
     {
@@ -304,6 +308,129 @@ module.exports = [
             else{
                 await interaction.reply({ embeds:[embed],components: [{type:1,components:[select[0]]},{type:1,components:[select[1]]},{type:1,components:[select[2]]},{type:1,components:[button]}]});
             }
+        }
+    },
+    {
+        data: new SlashCommandBuilder()
+            .setName('comment-timetable')
+            .setDescription('特定の日にコメントを追加します。(明日からの1週間は曜日で指定可)')
+            .addStringOption(option =>
+                option
+                    .setName('学科')
+                    .setDescription('学科を指定します')
+                    .setRequired(true)
+                    .addChoices(
+                        { name: 'M-機械工学科', value: '1' },
+                        { name: 'E-電気電子工学科', value: '2' },
+                        { name: 'D-電子制御工学科', value: '3' },
+                        { name: 'J-情報工学科', value: '4' },
+                        { name: 'C-環境都市工学科', value: '5' },
+                    )
+            )
+            .addStringOption(option =>
+                option
+                    .setName('学年')
+                    .setDescription('学年を指定します。')
+                    .setRequired(true)
+                    .addChoices(
+                        { name: '1年生', value: '1' },
+                        { name: '2年生', value: '2' },
+                        { name: '3年生', value: '3' },
+                        { name: '4年生', value: '4' },
+                        { name: '5年生', value: '5' },
+                    )
+            )
+            .addIntegerOption(option =>
+                option
+                    .setName('変更日')
+                    .setDescription('コメントを追加する日を、月×100+日でいれてください。例)12月14日→1214')
+                    .setRequired(false)
+            )
+            .addStringOption(option =>
+                option
+                    .setName('曜日')
+                    .setDescription('今日を除く直近1週間は曜日で指定できます。')
+                    .setRequired(false)
+                    .addChoices(
+                        { name: '月曜日', value: '1' },
+                        { name: '火曜日', value: '2' },
+                        { name: '水曜日', value: '3' },
+                        { name: '木曜日', value: '4' },
+                        { name: '金曜日', value: '5' },
+                    )
+            ),
+
+        async execute(interaction) {
+            await interaction.deferReply();
+            const grade = interaction.customId[0];
+            const department = interaction.customId[1];
+            const mode = interaction.customId.slice(-1);
+            const date = interaction.customId.substring(2,interaction.customId.match(/changeTimetableButton/).index);
+
+
+            let data = await db.find("main","timetableData",{day:date + '00'});
+
+            const modal = new ModalBuilder()
+                .setCustomId(`${data}addCommentTimetableModal${grade}${department}`)
+                .setTitle(`${Math.floor(date/100)}月${Math.floor(date%100)}日 - コメントを追加`);
+
+            for(let i = 0; i < data[0].timetable.length;i++){
+                const input = new TextInputBuilder()
+                    .setCustomId(`${data}addCommentTimetable${grade}${department}${i}`)
+                    .setLabel(`${2*i+1}-${2*i+2}限目(${data[0].timetable[i].name})のコメントを登録`)
+                    .setRequired(false)
+                    .setStyle(1);
+                modal.addComponents(new ActionRowBuilder().addComponents(input));
+            }
+            const input = new TextInputBuilder()
+                .setCustomId(`${data}addCommentTimetable${grade}${department}5`)
+                .setLabel(`${Math.floor(date/100)}月${Math.floor(date%100)}日の時間割にコメントを登録`)
+                .setStyle(1);
+            modal.addComponents(new ActionRowBuilder().addComponents(input));
+            await interaction.showModal(modal);
+            const filter = (mInteraction) => mInteraction.customId === `${data}addCommentTimetableModal${grade}${department}`;
+
+            interaction.awaitModalSubmit({ filter, time: 360000 })
+                .then(async mInteraction => {
+                    let inputTxt = [],comment;
+                    for (let i = 0; i < data[0].timetable.length; i++) {
+                        inputTxt[i] = mInteraction.fields.getTextInputValue(`${data}addCommentTimetable${grade}${department}${i}`);
+                    }
+                    comment = mInteraction.fields.getTextInputValue(`${data}addCommentTimetable${grade}${department}5`);
+
+
+                    for(let i = 0; i < data[0].timetable.length;i++){
+                        if(inputTxt[i]!=="" && inputTxt[i]!==undefined)data[0].timetable[i].comment = inputTxt[i];
+                    }
+                    data[0].comment = comment;
+                    data[0].day = date;
+                    if(mode === '0'){
+                        data[0].test = true;
+                    }
+                    else{
+                        data[0].test = false;
+                    }
+                    delete data[0]._id;
+                    await db.updateOrInsert("main","timetableData",{day:date},data[0]);
+                    await db.delete("main","timetableData",{day:date + '00'});
+                    const channel = client.channels.cache.get(interaction.message.channelId);
+                    channel.messages.fetch(interaction.message.id)
+                        .then((message) => {
+                            message.delete();
+                        })
+                        .catch(error => {
+
+                        });
+                    let replyOptions=time=>{return{content: '登録しました。\n(このメッセージは'+time+'秒後に自動で削除されます)', ephemeral:true};};
+                    await mInteraction.reply(replyOptions(5));
+                    for(let i=5;i>0;i--){
+                        await mInteraction.editReply(replyOptions(i));
+                        await setTimeout(1000);
+                    }
+                    await mInteraction.deleteReply();
+
+
+                })
         }
     }
 
