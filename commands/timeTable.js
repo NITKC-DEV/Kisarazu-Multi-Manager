9e238cb132e7015a26c65bc5e9af0be19a3375cf
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-18 13:18:20 +0900

    comment-timetableコマンドを完成させた、dbにincludesコマンドを実装させた。その他バグを修正

--- commands/timeTable.js
+++ commands/timeTable.js
@@ -340,17 +340,11 @@ module.exports = [
                         { name: '5年生', value: '5' },
                     )
             )
-            .addIntegerOption(option =>
-                option
-                    .setName('変更日')
-                    .setDescription('コメントを追加する日を、月×100+日でいれてください。例)12月14日→1214')
-                    .setRequired(false)
-            )
             .addStringOption(option =>
                 option
-                    .setName('曜日')
-                    .setDescription('今日を除く直近1週間は曜日で指定できます。')
-                    .setRequired(false)
+                    .setName('変更日')
+                    .setDescription('明日~来週の今日までを曜日で指定します(それ以上は管理者限定)')
+                    .setRequired(true)
                     .addChoices(
                         { name: '月曜日', value: '1' },
                         { name: '火曜日', value: '2' },
@@ -361,66 +355,62 @@ module.exports = [
             ),
 
         async execute(interaction) {
-            await interaction.deferReply();
-            const grade = interaction.customId[0];
-            const department = interaction.customId[1];
-            const mode = interaction.customId.slice(-1);
-            const date = interaction.customId.substring(2,interaction.customId.match(/changeTimetableButton/).index);
+            let nowDate = new Date();
+            let nowDay = nowDate.getDay(); //今日
+            let nextDay = parseFloat(interaction.options.getString('変更日')) - nowDay; //対象の曜日は何日後?
+            if(0 >= nextDay){nextDay += 7}
 
+            nowDate.setDate(nowDate.getDate() + nextDay);//対象の日を取得
+            let date = (nowDate.getMonth()+1)*100 + nowDate.getDate();
 
-            let data = await db.find("main","timetableData",{day:date + '00'});
+            const grade = interaction.options.getString('学年');
+            const department = interaction.options.getString('学科');
+            let data = await db.find("main","timetableData",{grade: grade,department: department,day: String(date)});
+
+            if(data.length === 0){
+                data = await db.find("main","timetableData",{grade: grade,department: department,day:interaction.options.getString('変更日')});
+            }
+
 
             const modal = new ModalBuilder()
-                .setCustomId(`${data}addCommentTimetableModal${grade}${department}`)
-                .setTitle(`${Math.floor(date/100)}月${Math.floor(date%100)}日 - コメントを追加`);
+                .setCustomId(`${date}addCommentTimetableModal${grade}${department}`)
+                .setTitle(`${departmentData[parseFloat(interaction.options.getString('学科'))-1].name}${interaction.options.getString('学年')}年${Math.floor(date/100)}月${Math.floor(date%100)}日`);
 
             for(let i = 0; i < data[0].timetable.length;i++){
                 const input = new TextInputBuilder()
-                    .setCustomId(`${data}addCommentTimetable${grade}${department}${i}`)
+                    .setCustomId(`${date}addCommentTimetable${grade}${department}${i}`)
                     .setLabel(`${2*i+1}-${2*i+2}限目(${data[0].timetable[i].name})のコメントを登録`)
                     .setRequired(false)
                     .setStyle(1);
                 modal.addComponents(new ActionRowBuilder().addComponents(input));
             }
             const input = new TextInputBuilder()
-                .setCustomId(`${data}addCommentTimetable${grade}${department}5`)
+                .setCustomId(`${date}addCommentTimetable${grade}${department}5`)
                 .setLabel(`${Math.floor(date/100)}月${Math.floor(date%100)}日の時間割にコメントを登録`)
+                .setRequired(false)
                 .setStyle(1);
             modal.addComponents(new ActionRowBuilder().addComponents(input));
             await interaction.showModal(modal);
-            const filter = (mInteraction) => mInteraction.customId === `${data}addCommentTimetableModal${grade}${department}`;
+            const filter = (mInteraction) => mInteraction.customId === `${date}addCommentTimetableModal${grade}${department}`;
 
             interaction.awaitModalSubmit({ filter, time: 360000 })
                 .then(async mInteraction => {
                     let inputTxt = [],comment;
                     for (let i = 0; i < data[0].timetable.length; i++) {
-                        inputTxt[i] = mInteraction.fields.getTextInputValue(`${data}addCommentTimetable${grade}${department}${i}`);
+                        inputTxt[i] = mInteraction.fields.getTextInputValue(`${date}addCommentTimetable${grade}${department}${i}`);
                     }
-                    comment = mInteraction.fields.getTextInputValue(`${data}addCommentTimetable${grade}${department}5`);
+                    comment = mInteraction.fields.getTextInputValue(`${date}addCommentTimetable${grade}${department}5`);
 
 
                     for(let i = 0; i < data[0].timetable.length;i++){
                         if(inputTxt[i]!=="" && inputTxt[i]!==undefined)data[0].timetable[i].comment = inputTxt[i];
                     }
-                    data[0].comment = comment;
-                    data[0].day = date;
-                    if(mode === '0'){
-                        data[0].test = true;
-                    }
-                    else{
-                        data[0].test = false;
-                    }
+                    if(comment!=="" && comment!==undefined)data[0].comment = comment;
+                    data[0].day = String(date);
                     delete data[0]._id;
-                    await db.updateOrInsert("main","timetableData",{day:date},data[0]);
-                    await db.delete("main","timetableData",{day:date + '00'});
-                    const channel = client.channels.cache.get(interaction.message.channelId);
-                    channel.messages.fetch(interaction.message.id)
-                        .then((message) => {
-                            message.delete();
-                        })
-                        .catch(error => {
 
-                        });
+                    await db.updateOrInsert("main","timetableData",{day:String(date)},data[0]);
+
                     let replyOptions=time=>{return{content: '登録しました。\n(このメッセージは'+time+'秒後に自動で削除されます)', ephemeral:true};};
                     await mInteraction.reply(replyOptions(5));
                     for(let i=5;i>0;i--){
@@ -428,8 +418,6 @@ module.exports = [
                         await setTimeout(1000);
                     }
                     await mInteraction.deleteReply();
-
-
                 })
         }
     }
