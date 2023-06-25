dabf90424a03cf6e2b70fb9c0f7ba741b7785317
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-25 14:12:50 +0900

    昨日の時間割臨時データを削除するように変更

--- botmain.js
+++ botmain.js
@@ -376,12 +376,13 @@ client.on('messageCreate', message => {
     }
 })
 
-/*誕生日通知とGuildDataチェック*/
-cron.schedule('0 0 * * *', async () => {
+/*誕生日通知とGuildDataチェック、時間割変更データチェック*/
+cron.schedule('12 14 * * *', async () => {
     await birthday.func();
     await system.log('誕生日お祝い！');
 
     await guildData.checkGuild();
+    await timetable.deleteData();
 });
 
 /*原神デイリー通知*/
