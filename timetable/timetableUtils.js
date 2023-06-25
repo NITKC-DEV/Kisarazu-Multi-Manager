2995735db9680a9da9c9b8170627ab95af9f5878
starkoka <j22437@inc.kisarazu.ac.jp>
2023-06-25 20:27:36 +0900

    URL書き換え

--- timetable/timetableUtils.js
+++ timetable/timetableUtils.js
@@ -4,8 +4,8 @@ const { EmbedBuilder } =require('discord.js')
 const tmp = new EmbedBuilder().setAuthor({
     name: "木更津高専統合管理BOT",
     iconURL: 'https://media.discordapp.net/attachments/1004598980929404960/1039920326903087104/nitkc22io-1.png',
-    url: 'https://github.com/NITKC22s/bot-main'
-}).setFooter({ text: 'Developed by NITKC22s server Admin' });
+    url: 'https://github.com/NITKC-DEV/Kisarazu-Multi-Manager'
+}).setFooter({ text: 'Developed by NITKC-DEV' });
 const dayOfWeeks = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
 module.exports = function timetableBuilder(classValue, dayOfWeek) {
     return tmp.setColor(classValue.color).setTitle(`${classValue.name} 時間割`)
