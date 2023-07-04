5a58c76bfb71115ca352334064ce7b130db61eb1
Kou-Ro <koushi.rockv@gmail.com>
2023-07-04 09:42:10 +0900

    iikannjinisita

--- functions/common.js
+++ functions/common.js
@@ -5,15 +5,17 @@ const {setTimeout} = require ("node:timers/promises");
  * @param interaction 返信するinteraction
  * @param options メッセージのオブジェクト(contentの中の$time$を現在の残り時間に置き換える)
  * @param time 返信してから削除するまでの時間
- * @returns {Promise<void>} void(同期処理)
- */
-exports.autoDeleteEditReply = async function(interaction,options,time) {
-    for(let i=time; i > 0;i--) {
-        await setTimeout(1000);
-        const defaultContent = JSON.parse(JSON.stringify(options)).content;
-        const sendingOptions = JSON.parse(JSON.stringify(options));
-        sendingOptions.content = defaultContent.replace(/\$time\$/g, i);
-        await interaction.editReply(sendingOptions);
-    }
-    await interaction.deleteReply();
+ * @returns {void} void
+*/
+exports.autoDeleteEditReply = function(interaction,options,time) {
+    (async()=> {
+        for(let i = time; i > 0; i--) {
+            const defaultContent = JSON.parse(JSON.stringify(options)).content;
+            const sendingOptions = JSON.parse(JSON.stringify(options));
+            sendingOptions.content = defaultContent.replace(/\$time\$/g, i);
+            await setTimeout(1000);
+            await interaction.editReply(sendingOptions);
+        }
+        await interaction.deleteReply();
+    })();
 }
