34e6e1d3a257dc149b9442606a7e50f9b2c95933
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-02-21 18:13:01 +0900

    require() -> readFileSync()

--- environmentConfig.js
+++ environmentConfig.js
@@ -1,6 +1,7 @@
-const Path = require("path");
+const fs = require("fs");
+const path = require("path");
 
-const configPath = Path.resolve(__dirname, process.env.NODE_ENV === "production" ? "./config.json" : "./config.dev.json");
-const config = require(configPath);
+const configPath = path.resolve(__dirname, process.env.NODE_ENV === "production" ? "./config.json" : "./config.dev.json");
+const config = await JSON.parse(fs.readFileSync(configPath, "utf-8"));
 module.exports = config;
 module.exports.configPath = configPath;
