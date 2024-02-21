72b860a71ee32de5ad8b38eaf7b3154dcdb240c1
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-02-21 18:43:28 +0900

    remove await

--- environmentConfig.js
+++ environmentConfig.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 const configPath = path.resolve(__dirname, process.env.NODE_ENV === "production" ? "./config.json" : "./config.dev.json");
-const config = await JSON.parse(fs.readFileSync(configPath, "utf-8"));
+const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
 module.exports = config;
 module.exports.configPath = configPath;
