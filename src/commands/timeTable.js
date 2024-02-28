0a217892ecb3e4131fb8515800cde8c312d84d72
NXVZBGBFBEN <107386214+NXVZBGBFBEN@users.noreply.github.com>
2024-02-28 23:30:40 +0900

    parseInt()してから評価するように変更
    - thx @naotiki
    
    Co-authored-by: Naotiki <razupai709@gmail.com>

--- src/commands/timeTable.js
+++ src/commands/timeTable.js
@@ -131,7 +131,7 @@ module.exports = [
                 grade = dt.getFullYear() - parseFloat(guildData[0].grade) + 1;
             }
 
-            if (Number.isNaN(grade)) {
+            if (Number.isNaN(parseInt(grade))) {
                 await interaction.editReply(
                     "このサーバーに学年情報が登録されていないため、学年オプションを省略できません。\n管理者に伝えて、/guilddataで入学した年を登録してもらってください。",
                 );
